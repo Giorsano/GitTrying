3148f98e81cfe4ed38b574cfc8983ff0e9375db2
Giordano Orlacchio <112937080+Giorsano@users.noreply.github.com>
2023-03-19 15:25:18 +0100

    Add files via upload

---
+++ ProvaGitModificheGoGo/src/main/java/com/example/ProvaGitModificheGoGo/ProvaGitModificheGoGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProvaGitModificheGoGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaGitModificheGoGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaGitModificheGoGoApplication.class, args);
+	}
+
+}
---
+++ ProvaGitModificheGoGo/src/test/java/com/example/ProvaGitModificheGoGo/ProvaGitModificheGoGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProvaGitModificheGoGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaGitModificheGoGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
